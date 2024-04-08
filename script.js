160f2a952fd8723918c815643426845d8de540c0
BigSteveXD <24504153+BigSteveXD@users.noreply.github.com>
2024-04-08 01:17:39 -0700

    added boolean variables to handle 2 or more operators pressed subsequently which caused errors

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 let displayVar = "0";
 let currentOp = add;
-let numA = 0;//null
-let numB = 0;//null
+let prevOp;
+let numA = 0;
+let numB = 0;
+
+let bSwitch = false;
+let bReset = false;
 
 function add(a, b){
     return a + b;
 }
-
 function subtract(a, b){
     return a - b;
 }
-
 function multiply(a, b){
     return a * b;
 }
-
 function divide(a, b){
     return a / b;
 }
-
 function operate(operator, num, num2){
     return operator(num, num2);
 }
 
-//console.log(operate(add, 3, 2));
 const display = document.getElementById("display");
 display.textContent = displayVar;
 
@@ -46,148 +45,247 @@ const divideBtn = document.getElementById("divide");
 const equalBtn = document.getElementById("equal");
 const clearBtn = document.getElementById("clear");
 
+
 zeroBtn.addEventListener('click', () => {
+    console.log("zeroBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         //do nothing
     }else{
         displayVar = displayVar + "0";
-        display.textContent = displayVar;
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 oneBtn.addEventListener('click', () => {
+    console.log("oneBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
-        displayVar="1";
+        displayVar = "1";
     }else{
         displayVar = displayVar + "1";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 twoBtn.addEventListener('click', () => {
+    console.log("twoBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="2";
     }else{
         displayVar = displayVar + "2";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 threeBtn.addEventListener('click', () => {
+    console.log("threeBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="3";
     }else{
         displayVar = displayVar + "3";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 fourBtn.addEventListener('click', () => {
+    console.log("fourBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar==0){
         displayVar="4";
     }else{
         displayVar = displayVar + "4";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 fiveBtn.addEventListener('click', () => {
+    console.log("fiveBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="5";
     }else{
         displayVar = displayVar + "5";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 sixBtn.addEventListener('click', () => {
+    console.log("sixBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="6";
     }else{
         displayVar = displayVar + "6";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 sevenBtn.addEventListener('click', () => {
+    console.log("sevenBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="7";
     }else{
         displayVar = displayVar + "7";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 eightBtn.addEventListener('click', () => {
+    console.log("eightBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="8";
     }else{
         displayVar = displayVar + "8";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 nineBtn.addEventListener('click', () => {
+    console.log("nineBtn");
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
     if(displayVar=="0"){
         displayVar="9";
     }else{
         displayVar = displayVar + "9";
     }
     display.textContent = displayVar;
-    console.log(displayVar);
+    console.log("displayVar: " + displayVar);
 });
 
-addBtn.addEventListener('click', () => {
+
+addBtn.addEventListener('click', () => {                                         
     console.log("add");
+    //if(prevOp==null){
+        //prevOp = currentOp;
+    //}else{
+        //prevOp = currentOp;
+    //}
+    prevOp = currentOp;
     currentOp = add;
-    if(numA=="0"){//==null
+    if(bSwitch==false){
         numA = Number(displayVar);
-        displayVar = "0";
+        bReset = true;
+        bSwitch = true;
+    }else if(bSwitch==true){
+        numB = Number(displayVar);
+        numA = operate(prevOp, numA, numB);
+        displayVar = numA;
         display.textContent = displayVar;
+        bReset = true;
     }
 });
 subtractBtn.addEventListener('click', () => {
     console.log("subtract");
+    prevOp = currentOp;
     currentOp = subtract;
-    if(numA=="0"){
+    if(bSwitch==false){
         numA = Number(displayVar);
-        displayVar = "0";
+        bReset = true;
+        bSwitch = true;
+    }else if(bSwitch==true){
+        numB = Number(displayVar);
+        numA = operate(prevOp, numA, numB);
+        displayVar = numA;
         display.textContent = displayVar;
+        bReset = true;
     }
 });
 multiplyBtn.addEventListener('click', () => {
     console.log("multiply");
+    prevOp = currentOp;
     currentOp = multiply;
-    if(numA=="0"){
+    if(bSwitch==false){
         numA = Number(displayVar);
-        displayVar = "0";
+        bReset = true;
+        bSwitch = true;
+    }else if(bSwitch==true){
+        numB = Number(displayVar);
+        numA = operate(prevOp, numA, numB);
+        displayVar = numA;
         display.textContent = displayVar;
+        bReset = true;
     }
 });
 divideBtn.addEventListener('click', () => {
     console.log("divide");
+    prevOp = currentOp;
     currentOp = divide;
-    if(numA=="0"){
+    if(bSwitch==false){
         numA = Number(displayVar);
-        displayVar = "0";
+        bReset = true;
+        bSwitch = true;
+    }else if(bSwitch==true){
+        numB = Number(displayVar);
+        numA = operate(prevOp, numA, numB);
+        displayVar = numA;
         display.textContent = displayVar;
+        bReset = true;
     }
 });
+
 
 equalBtn.addEventListener('click', () => {
     console.log("equal");
     numB = Number(displayVar);
-    console.log(operate(currentOp, numA, numB));
-    displayVar = operate(currentOp, numA, numB).toString();
+    if(bReset==true){
+        displayVar="0";
+        bReset = false;
+    }
+    bSwitch = false;
+    if(displayVar.localeCompare("Can't divide by zero")===0){
+        console.log("display reset");
+        numB = 0;
+        displayVar = "0";
+    }
+    if(numB==0 && currentOp == divide){
+        console.log("divide by zero");
+        displayVar = "Can't divide by zero";
+    }else{
+        console.log(operate(currentOp, numA, numB));
+        displayVar = operate(currentOp, numA, numB).toString();
+    }
     display.textContent = displayVar;
 });
 clearBtn.addEventListener('click', () => {
     console.log("clear");
     displayVar = "0";
-    numA = 0;//null
-    numB = 0;//null
+    numA = 0;
+    numB = 0;
     display.textContent = displayVar;
 });
-
 
